122b64823d722c5a92a13ca43a6d848079188fe4
Paruyr Danielian <paruyr.adian@gmail.com>
2016-06-06 14:10:10 +0400

    Initial Commit

---
+++ iefa/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iefa
+//
+//  Created by Paruyr Danielian on 6/6/16.
+//  Copyright © 2016 Paruyr Danielian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
