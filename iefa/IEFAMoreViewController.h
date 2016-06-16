75b5cdb72ba6980e6ae0ad5a1e420279caa68d05
Arman Markosyan <arman.markosyan@picsart.com>
2016-06-16 03:14:51 -0700

    Edited dictionary

---
+++ iefa/IEFAMoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  IEFAMoreViewController.h
+//  iefa
+//
+//  Created by Khoren Markosyan on 16.06.16.
+//  Copyright © 2016 Paruyr Danielian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IEFAMoreViewController : UIViewController
+
+@end
