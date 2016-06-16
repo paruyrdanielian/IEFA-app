7afa9bb1edb26800582f51daeefae4c01d6c5ae0
Sanasar <sanasar@inbox.ru>
2016-06-16 15:20:03 +0400

    added DB for random fact

---
+++ iefa/IEFARandomFactDB.m
@@ -0,0 +1,76 @@
+//
+//  IEFARandomFactDB.m
+//  iefa
+//
+//  Created by User on 6/16/16.
+//  Copyright © 2016 Paruyr Danielian. All rights reserved.
+//
+
+#import "IEFARandomFactDB.h"
+
+@implementation IEFARandomFactDB
+
+- (NSArray *)randomFacts {
+    return @[ @{@"image" : @"",
+                @"title" : @"Swan Lake",
+                @"about" : @"Kanye West gave a free concert here and even swam in the actual lake last year, when he and Kim Kardashian visited Armenia within the framework of events dedicated to the centennial of Armenian Genocide.\nP.S. Rumors say he even smiled……a few times….. "},
+              @{@"image" : @"",
+                @"title" : @"Opera",
+                @"about" : @"Since it was opened , the Armenian National Opera & Ballet Theatre has performed more than 200 different operas and ballets by Armenian, Russian and Western European composers (including ‘Carmen’, ‘Swan lake’, ‘La Traviata’ etc.) .And in case you were wondering, no, we can’t afford to hold the euroconcert here )))"},
+              @{@"image" : @"",
+                @"title" : @"Northern Avenue",
+                @"about" : @"is a pedestrian avenue that is mainly home to luxurious residential buildings, high-end brand shops, commercial offices, coffee shops etc"},
+              @{@"image" : @"",
+                @"title" : @"Republic Square",
+                @"about" : @"It was here that the world- famous Armenian rock band System of a Down performed last year, dedicating the concert to the centennial of the Armenian Genocide  KAM  The square is famous for its singing fountains that  perform a light show featuring classical music, jazz, pop ,rock. Both the fountains and their colors change according to the rhythm of the song or the music played.\nP.S. Armenia Marriott hotel, where Kim Kardashian stayed last year, is also situated here :D"},
+              @{@"image" : @"",
+                @"title" : @"Lovers’ park",
+                @"about" : @"The park is a favorite rendezvous for many couples, hence the name. Its design combines Japanese landscaping principles with the spirit of Armenia."},
+              @{@"image" : @"",
+                @"title" : @"Tsitsernakaberd",
+                @"about" : @"(literally \"swallow's fortress\") - Armenian Genocide Memorial Complex is situated here ; over the years, a wide range of famous political  figures have visited the memorial ,including the presidents of Russia, France, Ukraine, Greece etc."},
+              @{@"image" : @"",
+                @"title" : @"Victory Park",
+                @"about" : @" The park is named Victory Park in commemoration of Soviet Armenia's participation in the second World War. Mother Armenia Statue, which is situated here, is the female personification of Armenia ( Behave yourself around her , this lady comes alive to punish whomever she doesn’t like )))"},
+              @{@"image" : @"",
+                @"title" : @"Blue Mosque",
+                @"about" : @"Built in the 18th century in the country that was the first to adopt Christianity in the world, the mosque is a good example of our tolerance and acceptance of multiculturalism"},
+              @{@"image" : @"",
+                @"title" : @"The Cascade",
+                @"about" : @"A grandiose staircase that connects the upper and lower levels of the city and overviews Yerevan. We dare you to climb those 572, if you don’t die your resolution will pass )))"},
+              @{@"image" : @"",
+                @"title" : @" St. Anna church, Katoghike",
+                @"about" : @"A medieval chapel that dates back to the 13th century is surrounded by a modern religious complex right in the heart of the city – Abovyan street."},
+              @{@"image" : @"",
+                @"title" : @"Moscow cinema (Charles Aznavour square)",
+                @"about" : @"In the square named after legendary French-Armenian singer Charles Aznavour, you could find the most famous Armenian cinema that hosts numerous film festivals. Left  from Soviet times."},
+              @{@"image" : @"English park",
+                @"title" : @"Swan Lake",
+                @"about" : @"A park that unites Armenia, Italy, France and England. Many EYP teambuilding activities have taken place in this park. It is located on Italy street, French embassy and Sundukian theatre are situated there."},
+              @{@"image" : @"",
+                @"title" : @"Vernissage",
+                @"about" : @"An iconic open-air market-boulevard that unites artisans, painters and weavers of Armenia and is a number one destination for any tourist willing to obtain a souvenir from Armenia.( Make sure to buy one, we like tourists who spend money here :)"},
+              @{@"image" : @"",
+                @"title" : @"St. Gregory the Illuminator Cathedral",
+                @"about" : @"The largest cathedral of Armenian  Apostlic church in the world. St. Gregory the Illuminator is credited with converting Armenia from paganism to Christianity in 301. Armenia thus became the first nation to adopt Christianity as its official religion (A fact that we are going to repeat until you’re sick of it ) "},
+              @{@"image" : @"",
+                @"title" : @"Yerevan brandy factory (Ararat)",
+                @"about" : @"When Winston Churchill was asked about the secret of his longevity, he answered without hesitation: \"Never be late for dinner, smoke Hawaiian cigars and drink Armenian cognac!\". It was Ararat brandy factory that produced his favorite cognac Dvin ( And no, of course , you can’t afford to drink it :)"},
+              @{@"image" : @"",
+                @"title" : @"Constitutional Court",
+                @"about" : @"The Constitutional Court of the Republic of Armenia is the highest legal body for constitutional review in Armenia. It is a judicial body which is separate and independent from the executive, the legislative and the judiciary. Here is a quick representation of the way the system here works: What do you call a lawyer with an IQ of 40? Your Honour."},
+              @{@"image" : @"",
+                @"title" : @"National Assembly",
+                @"about" : @"National Assembly of Armenia is the legislative branch of the government of Armenia. It's not far from the Constitutional Court or the White House which makes the complication of our lives easier."},
+              @{@"image" : @"",
+                @"title" : @"Government building",
+                @"about" : @"The Government of Armenia or the executive branch of the Armenian government is an executive council of government ministers in Armenia. Bearing in mind that the people working here always used to be late it was decided to put a huge clock on the building. (Nothing changed and they still are late)."},
+              @{@"image" : @"",
+                @"title" : @"Central Bank",
+                @"about" : @"The CBA is an independent institution responsible for issuing all banknotes and coins in the country, overseeing and regulating the banking sector and keeping the government's currency reserves. We know what your first thought was. No. Robbing it is not a good idea."},
+              @{@"image" : @"",
+                @"title" : @"Shahumyan Statue",
+                @"about" : @"Stepan Georgevich Shaumian was a Bolshevik revolutionary and politician active throughout the Caucasus. Tips on how not to use the statue: 1. You can't use it as a toilet unless you're a bird. 2. You can't play with its feelings. It's not stone hearted like you. 3. You can't play the \"not blinking\" game, cause believe us, it'll win. We tried. Maybe. 4. You can't use pickup lines, although you can buy it a drink if it wants you to."}];
+}
+
+@end
