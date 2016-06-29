3f3223c4caac34e64ad61ead08c50d06eb4852e6
Paruyr Danielian <paruyr.adian@gmail.com>
2016-06-29 18:04:17 +0200

    added button class

---
+++ iefa/buttonWithCornerRadius.h
@@ -0,0 +1,15 @@
+//
+//  buttonWithCornerRadius.h
+//  UIForInnoma
+//
+//  Created by User on 6/23/16.
+//  Copyright © 2016 ACA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+IB_DESIGNABLE
+
+@interface buttonWithCornerRadius : UIButton
+
+@end
